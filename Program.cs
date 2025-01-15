cdcb1773878195cc6fad17eaa2d5fa03da783b1e
McChug <mcchug2112@gmail.com>
2025-01-14 19:32:40 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Mission2_Dice;
+
+internal class Program
+{
+    private static void Main()
+    {
+        RollDice rd = new RollDice();
+
+        // Run opening statements and collect user input
+        System.Console.WriteLine("Welcome to the dice throwing simulator!");
+        System.Console.WriteLine("How many dice rolls would you like to simulate?");
+        int number_of_rolls = int.Parse(System.Console.ReadLine());
+
+        // Run the Roll() method using the user's number of rolls
+        int[] roll_results = rd.Roll(number_of_rolls);
+
+        // Display results by looping through array
+        for (int i = 0; i < roll_results.Length; i++)
+        {
+            // get rounded number to represent percent
+            double percent_of_total = Math.Round(
+                (roll_results[i] / (double)number_of_rolls) * 100,
+                2
+            );
+            // build the histogram bars
+            string hist_bar = "";
+            for (int j = 0; j < Math.Floor(percent_of_total); j++)
+            {
+                hist_bar += "*";
+            }
+            // Print the number and bar
+            Console.WriteLine((i + 2) + ": " + hist_bar);
+        }
+
+        System.Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+    }
+}
---
+++ RollDice.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2_Dice
+{
+    internal class RollDice
+    {
+        public int[] Roll(int number_of_rolls)
+        {
+            // Initialize random
+            Random rnd = new Random();
+
+            // Init variables
+            int roll_1_result = 0;
+            int roll_2_result = 0;
+            int roll_total = 0;
+            int[] results = new int[11];
+
+            // Loop to simulate number_of_rolls
+            for (int i = 0; i < number_of_rolls; i++)
+            {
+                // Get two random dice rolls and add them together
+                roll_1_result = rnd.Next(1, 7);
+                roll_2_result = rnd.Next(1, 7);
+                roll_total = roll_1_result + roll_2_result;
+
+                // Store in array
+                results[roll_total - 2] += 1;
+            }
+
+            return results;
+        }
+    }
+}
